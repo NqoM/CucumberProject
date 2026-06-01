26492c1c9d0bd73a1366685793a381f7db0917e1
Nqobile Mgobozi <nqobile.mgobozi@gamesglobal.com>
2026-06-01 16:09:56 +0200

    added extent reports dependencies and extend properties file

--- src/test/java/Steps/Base.java
+++ src/test/java/Steps/Base.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Base {
+
     BrowserFactory browserFactory = new BrowserFactory();
     final WebDriver driver = browserFactory.startBrowser("chrome",
             "https://ndosisimplifiedautomation.vercel.app/");
